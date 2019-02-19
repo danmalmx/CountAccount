2fbe5b0b2483b476b25d6f2dd4ea99ac1ec45d5a
danmalmx <danmalmx@gmail.com>
2019-02-19 20:56:01 +0100

    adding smooth scrolling and transparent navbar

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,26 @@
     });
     const marker = new google.maps.Marker({position: loc, map: map});
   }
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#nav').style.opacity = 0.9;
+    } else {
+      document.querySelector('"nav').style.opacity = 1;
+    }
+  });
+
+$('#nav a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
