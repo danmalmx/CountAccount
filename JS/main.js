b5843ca5f7767b63a092feda67fa86e632ce426a
danmalmx <danmalmx@gmail.com>
2019-02-19 20:30:01 +0100

    adding contact section, including contact form, map and styling

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,9 @@
-  
+  function initMap() {
+    const loc = {lat: 57.708870, lng: 11.974560};
+    const map = new google.maps.Map(document.querySelector('.map')
+    ,{
+      zoom: 14,
+      center: loc
+    });
+    const marker = new google.maps.Marker({position: loc, map: map});
+  }
